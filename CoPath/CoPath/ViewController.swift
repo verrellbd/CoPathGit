c8a356f5d2d5040ad93fa55dbef0f0a60dc00879
Henkky <henkky888@gmail.com>
2019-05-02 14:34:49 +0700

    init

---
+++ CoPath/CoPath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoPath
+//
+//  Created by Henkky on 02/05/19.
+//  Copyright © 2019 Henkky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
