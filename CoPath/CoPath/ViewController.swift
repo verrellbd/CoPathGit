73edc9dd89a80bf8c89fb01fe0b1c9b3228cfffc
deanverrell <deanverrell@gmail.com>
2019-05-02 15:10:23 +0700

    anti

--- CoPath/CoPath/ViewController.swift
+++ CoPath/CoPath/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         print("abc")
+        print("asdfgh")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
