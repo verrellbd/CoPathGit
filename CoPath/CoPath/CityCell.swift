79bfeaebc5481aff6fb61079780e81afb7459b1a
deanverrell <deanverrell@gmail.com>
2019-05-03 22:38:30 +0700

    nknkldnls

---
+++ CoPath/CoPath/CityCell.swift
@@ -0,0 +1,32 @@
+//
+//  CityCell.swift
+//  CoPath
+//
+//  Created by Dean Verrell on 03/05/19.
+//  Copyright © 2019 Henkky. All rights reserved.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+
+    @IBOutlet weak var imageCell3: UIImageView!
+    @IBOutlet weak var imageCell2: UIImageView!
+    @IBOutlet weak var imageCell1: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+    func setImage(image: Image) {
+        imageCell1.image = image.image1
+        imageCell2.image = image.image2
+        imageCell3.image = image.image3
+    }
+
+}
--- CoPath/CoPath/CityViewController.swift
+++ CoPath/CoPath/CityViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 
 class CityViewController: UIViewController {
     @IBOutlet weak var cityList: UITableView!
-    var image : [Image] = []
+    var images : [Image] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        images = createArray()
         // Do any additional setup after loading the view.
     }
     
+    func createArray() -> [Image]{
+        let images1 = Image(image1: #imageLiteral(resourceName: "beginner-first-app"), image2: #imageLiteral(resourceName: "beginner-first-app"), image3: #imageLiteral(resourceName: "beginner-first-app"))
+        let images2 = Image(image1: #imageLiteral(resourceName: "beginner-first-app"), image2: #imageLiteral(resourceName: "beginner-first-app"), image3: #imageLiteral(resourceName: "beginner-first-app"))
+        let images3 = Image(image1: #imageLiteral(resourceName: "beginner-first-app"), image2: #imageLiteral(resourceName: "beginner-first-app"), image3: #imageLiteral(resourceName: "beginner-first-app"))
+        return [images1,images2,images3]
+    }
 
     /*
     // MARK: - Navigation
@@ -27,5 +34,20 @@ class CityViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension CityViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let image = images[indexPath.row]
+        let cell = cityList.dequeueReusableCell(withIdentifier: "CityCell") as! CityCell
+        cell.setImage(image: image)
+        
+        return cell
+    }
+    
+    
 }
