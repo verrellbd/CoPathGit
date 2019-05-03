62dba281dcac8895867a3a61d0fd1558967672d8
deanverrell <deanverrell@gmail.com>
2019-05-03 14:24:57 +0700

    sa

---
+++ CoPath/CoPath/addPlanController.swift
@@ -0,0 +1,34 @@
+//
+//  addPlanController.swift
+//  CoPath
+//
+//  Created by Dean Verrell on 03/05/19.
+//  Copyright © 2019 Henkky. All rights reserved.
+//
+
+import UIKit
+
+class addPlanController: UIViewController {
+
+    @IBOutlet weak var startField: UITextField!
+    
+    
+    @IBOutlet weak var endDate: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
