942d1fee53a15f346faf29231606221f63384a38
Henkky <henkky888@gmail.com>
2019-05-04 06:26:00 +0700

    image Slider feature

---
+++ CoPath/CoPath/PlaceRecommendationCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlaceRecommendationCell.swift
+//  CoPath
+//
+//  Created by Henkky on 04/05/19.
+//  Copyright © 2019 Henkky. All rights reserved.
+//
+
+import UIKit
+
+class PlaceRecommendationCell: UICollectionViewCell {
+    @IBOutlet weak var img: UIImageView!
+}
