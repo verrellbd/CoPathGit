65465bc32b8367b3b158cf342c2d12b8b2a42d01
Verine <verinesulin1999@gmail.com>
2019-05-03 20:58:59 +0700

    file swift plan

---
+++ CoPath/CoPath/PlanViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlanViewController.swift
+//  CoPath
+//
+//  Created by verine on 03/05/19.
+//  Copyright © 2019 Henkky. All rights reserved.
+//
+
+import UIKit
+
+class PlanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
