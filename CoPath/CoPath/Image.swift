f3be5ead6c28d69bc2da9d37f87168a2b234ae44
deanverrell <deanverrell@gmail.com>
2019-05-03 22:16:22 +0700

    dsadsadsa

---
+++ CoPath/CoPath/Image.swift
@@ -0,0 +1,22 @@
+//
+//  Image.swift
+//  CoPath
+//
+//  Created by Dean Verrell on 03/05/19.
+//  Copyright © 2019 Henkky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Image {
+    let image1 : UIImage
+    let image2 : UIImage
+    let image3 : UIImage
+    
+    init(image1 : UIImage,image2 : UIImage,image3 : UIImage) {
+        self.image1 = image1
+        self.image2 = image2
+        self.image3 = image3
+    }
+}
