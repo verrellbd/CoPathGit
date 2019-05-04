8ad6fde07891ecdab57bfe0b80084e93c69cb95c
deanverrell <deanverrell@gmail.com>
2019-05-04 22:00:57 +0700

    nisdaksa

---
+++ CoPath/CoPath/Add Plan/ListDestination.swift
@@ -0,0 +1,9 @@
+//
+//  ListDestination.swift
+//  CoPath
+//
+//  Created by Dean Verrell on 04/05/19.
+//  Copyright © 2019 Henkky. All rights reserved.
+//
+
+import Foundation
---
+++ CoPath/CoPath/Add Plan/ListDestinationCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListDestinationCell.swift
+//  CoPath
+//
+//  Created by Dean Verrell on 04/05/19.
+//  Copyright © 2019 Henkky. All rights reserved.
+//
+
+import UIKit
+
+class ListDestinationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
