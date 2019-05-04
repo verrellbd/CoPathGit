70af395bc18391c2637ab05c76188d3daddd8b8a
deanverrell <deanverrell@gmail.com>
2019-05-04 22:06:56 +0700

    sad

--- CoPath/CoPath/Add Plan/ListDestination.swift
+++ CoPath/CoPath/Add Plan/ListDestination.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+import UIKit
+
+class List {
+    let title : String
+    let time : String
+    
+    init(title:String,time:String) {
+        self.time = time
+        self.title = title
+    }
+    
+}
