4efd9a4c760e437f2296cb0315d0e63b359b7a33
verine <verinesulin1999@gmail.com>
2019-05-04 15:55:15 +0700

    add notification in appDelegate

--- CoPath/CoPath/AppDelegate.swift
+++ CoPath/CoPath/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) {
+            (granted, error) in print("granted:\(granted)")
+        }
         return true
     }
 
