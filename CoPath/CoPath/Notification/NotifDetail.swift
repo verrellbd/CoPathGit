211177d1445a9262faddfa404392d080675e3e4d
deanverrell <deanverrell@gmail.com>
2019-05-05 21:02:26 +0700

    dasdsadas

---
+++ CoPath/CoPath/Notification/NotifDetail.swift
@@ -0,0 +1,31 @@
+//
+//  NotifDetail.swift
+//  CoPath
+//
+//  Created by Dean Verrell on 05/05/19.
+//  Copyright © 2019 Henkky. All rights reserved.
+//
+
+import UIKit
+
+class NotifDetail: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
