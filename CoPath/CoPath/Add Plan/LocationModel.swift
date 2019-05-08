7565d50f3b1b3173943612f3089d817b56cf6644
deanverrell <deanverrell@gmail.com>
2019-05-08 09:55:26 +0700

    dsajsd

---
+++ CoPath/CoPath/Add Plan/LocationModel.swift
@@ -0,0 +1,13 @@
+//
+//  LocationModel.swift
+//  CoPath
+//
+//  Created by Dean Verrell on 08/05/19.
+//  Copyright © 2019 Henkky. All rights reserved.
+//
+
+import Foundation
+
+struct LocationModel {
+    static var location = [CoordinateModel]()
+}
